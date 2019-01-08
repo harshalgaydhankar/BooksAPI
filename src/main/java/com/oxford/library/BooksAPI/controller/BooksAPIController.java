09314940a6c781b4e2c42aa8583387148581f9d1
Harshal <bukha@allstate.com>
2019-01-08 19:29:50 +0530

    added GET and POST endpoints

--- src/main/java/com/oxford/library/BooksAPI/controller/BooksAPIController.java
+++ src/main/java/com/oxford/library/BooksAPI/controller/BooksAPIController.java
@@ -1,10 +1,25 @@
 package com.oxford.library.BooksAPI.controller;
 
+import com.oxford.library.BooksAPI.model.Book;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/books")
 public class BooksAPIController {
 
+    private List<Book> books = new ArrayList<>();
+
+    @GetMapping("/getBooks")
+    public List<Book> getBooks(){
+        return books;
+    }
+
+    @PostMapping("/addBook")
+    public String addBook(@RequestBody Book book){
+        books.add(book);
+        return "Book has been added in library";
+    }
 }
