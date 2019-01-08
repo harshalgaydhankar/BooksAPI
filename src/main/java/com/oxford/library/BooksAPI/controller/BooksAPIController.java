9f4e80177bf15f3bc077f3a2b940269cb6ab3fa5
Harshal <bukha@allstate.com>
2019-01-08 19:28:39 +0530

    added Book POJO

--- src/main/java/com/oxford/library/BooksAPI/controller/BooksAPIController.java
+++ src/main/java/com/oxford/library/BooksAPI/controller/BooksAPIController.java
@@ -1,13 +1,10 @@
 package com.oxford.library.BooksAPI.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("api/books")
 public class BooksAPIController {
-
-
 
 }
--- src/main/java/com/oxford/library/BooksAPI/model/Book.java
+++ src/main/java/com/oxford/library/BooksAPI/model/Book.java
@@ -1,3 +1,12 @@
 package com.oxford.library.BooksAPI.model;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
 public class Book {
+    private int bookId;
+    private String bookName;
+    private String authorName;
 }
